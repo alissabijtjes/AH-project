b48c5eca45ee54e3234bf844ab593a30c5a4dd60
Alissa Bijtjes <aliss@MacBook-Air-van-Alissa.local>
2023-06-12 13:54:02 +0200

    route.py toegevoegd.

--- code/station.py
+++ code/station.py
@@ -15,21 +15,5 @@ class Station():
     def add_connection(self, destination, time):
         """"""
         self.connections.append((destination, time))
-        
-class Route():
-    """Class route."""
-    counter = 1
-    def __init__(self, start):
-        self.id = Route.counter
-        self.route = [start]
-        self.total_time = 0
-        Route.counter += 1
 
-    def add_route(self, destination, time):
-        self.route.append(destination)
-        self.total_time += time
-
-    def delete_route(self, destination, time):
-        self.route.remove(destination)
-        self.total_time -= time
 
