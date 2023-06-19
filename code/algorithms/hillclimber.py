c01f3117c81eec83fde1b7275fbbb411df0d48a3
Alissa Bijtjes <alissa.bijtjes@student.uva.nl>
2023-06-20 00:39:34 +0200

    aanpassingen hillclimber p-waarde

--- code/algorithms/hillclimber.py
+++ code/algorithms/hillclimber.py
@@ -7,7 +7,6 @@ import random
 from code.classes.route import Route
 from code.classes.station import Station
 import copy
-from resultaten.write_results import write_results
 
 # optie heuristiek: laatste station weghalen (bv. Alkmaar-Den Helder-Alkmaar)
 
@@ -45,11 +44,12 @@ def hillclimber(map, original_all_routes, copy_all_stations, all_stations, var_m
     # voor de overige tracks de connecties van route.route naar copy_all_stations
     new_all_routes = []
 
+    # Sets all connections to false
+    for station in copy_all_stations:
+        for connection in station.connections:
+            connection[2] = False
+
     for route in original_all_routes:
-        #i = original_all_routes.index(route)
-        #if i is not index:
-            #print("----------------treinbegin", route.route[0].name)
-            #print(copy_all_stations)
             for station in copy_all_stations:
                 if station.name == route.route[0].name:
                     start_station = station
@@ -71,41 +71,15 @@ def hillclimber(map, original_all_routes, copy_all_stations, all_stations, var_m
                                 new_route.add_route(station, time)
                                 # Set connections to ridden
                                 station.ridden_connection(route_station)
-                                route_station.ridden_connection(station)
-
-
-                        # station.connections = route_station.connections
-
-    # # Set every connection to false
-    # for station in copy_all_stations:
-    #     for connection in station.connections:
-    #         connection[2] = False
-
-    # # Set if connections are made to true, not the place where the new random route is getting placed
-    # for route in new_all_routes:
-    #     i = new_all_routes.index(route)
-    #     if i != index:
-    #         #print("----------------treinbegin", route.route[0].name)
-    #         j = 0
-    #         for j in range(len(route.route) - 1):
-    #             current_station = route.route[j]
-    #             next_station = route.route[j + 1]
-    #             for station in copy_all_stations:
-    #                 if station.name == current_station.name:
-    #                     for connection in station.connections:
-    #                         if connection[0].name == next_station.name:
-    #                             connection[2] = True                    
+                                route_station.ridden_connection(station)                   
+    
+    # Variabel p before adding new route
+    original_var_p = random_run.fraction_p(copy_all_stations)
 
     # new random route
     new_route = random_run.route_(copy_all_stations, map)
     #new_route = greedy.greedy(copy_all_stations, map)
-    min_new_route = new_route.total_time
 
-    # op plek van current route de nieuwe route
-    # print(original_all_routes)
-    # print(index)
-    # print(new_route)
-    # original_all_routes[index] = new_route
     new_all_routes = copy.deepcopy(original_all_routes)
     new_all_routes[index] = new_route
 
@@ -116,9 +90,8 @@ def hillclimber(map, original_all_routes, copy_all_stations, all_stations, var_m
 
     # Variabels calculation
     var_t = len(new_all_routes)
-    #print("t-waarde", var_t)
     var_p = random_run.fraction_p(copy_all_stations)
-    #print("p-waarde", var_p) 
+    #print(var_p)
     var_min_totaal = var_min_new
     #var_min_totaal = var_min_first - min_current_route + min_new_route
     #print("min-waarde", var_min_totaal)
@@ -126,11 +99,13 @@ def hillclimber(map, original_all_routes, copy_all_stations, all_stations, var_m
     print("k-waarde", var_k)
     print("current k", current_k)
 
-    # wel alle stations ook bereikt
+    # Compare current k-value with new k-value
     if var_k > current_k:
         original_all_routes = new_all_routes
         current_k = var_k
         var_min = var_min_totaal
+    else:
+        var_p = original_var_p
 
     values_list = [var_p, var_t, var_min, current_k]
 
